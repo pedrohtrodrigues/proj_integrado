51eb7582a691335c56816ba4072836978d92b8dc
Pedro HT <pedrohtr@hotmail.com>
2024-09-03 16:52:12 -0300

    Atualizacao HTML e CSS dos videos 03/09/2024

---
+++ java/videos.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    src: "../videos/01.mp4",
+    title: "Editando fotos",
+  },
+  {
+    src: "../videos/02.mp4",
+    title: "Live Stream",
+  },
+  {
+    src: "../videos/03.mp4",
+    title: "Digitando",
+  },
+  {
+    src: "../videos/04.mp4",
+    title: "Home office",
+  },
+  {
+    src: "../videos/06.mp4",
+    title: "Gota de água",
+  },
+  {
+    src: "../videos/07.mp4",
+    title: "Código",
+  },
+];
